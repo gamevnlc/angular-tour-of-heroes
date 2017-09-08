a63ee0100bccd9f469fccd4b8c79ae1f6fd0ae23
HuyPham <pham.xuan.huy@mqsolutions.vn>
2017-09-08 14:34:37 +0700

    changed favicon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap'; // Bootstrap
 import { FormsModule } from '@angular/forms'; // NgModel lives here
-import { RouterModule } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroService } from './hero.service';
 import { DashboardComponent } from './dashboard.component';
-
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -22,21 +22,7 @@ import { DashboardComponent } from './dashboard.component';
     BrowserModule,
     NgbModule.forRoot(),
     FormsModule, // <-- Import the FormsModule before binding with [(ngModel)],
-    RouterModule.forRoot([
-      {
-        path: 'heroes',
-        component: HeroesComponent
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      },
-      {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full'
-      }
-    ])
+    AppRoutingModule
   ],
   providers: [HeroService],
   bootstrap: [AppComponent]
--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,5 +1,12 @@
-import { Component, Input } from '@angular/core';
+import 'rxjs/add/operator/switchMap';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Location } from '@angular/common';
+
+
+
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 
 
@@ -8,6 +15,24 @@ import { Hero } from './hero';
     templateUrl: './hero-detail.component.html'
 })
 
-export class HeroDetailComponent {
+export class HeroDetailComponent implements OnInit {
     @Input() hero: Hero;
+
+    constructor(
+        private heroService: HeroService,
+        private route: ActivatedRoute,
+        private location: Location
+    ) {}
+
+    ngOnInit(): void {
+        this.route.paramMap
+        .switchMap(
+            (params: ParamMap) => this.heroService.getHero(+params.get('id'))
+        )
+        .subscribe(hero => this.hero = hero);
+    }
+
+    goBack(): void {
+        this.location.back();
+    }
 }
