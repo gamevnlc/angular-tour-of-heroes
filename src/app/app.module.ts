22cf898d256e96aabd21facd60a6c3ce998477f2
HuyPham <gamevnlc1591@gmail.com>
2017-10-03 18:01:19 +0700

    http module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap'; // Bootstrap
 import { FormsModule } from '@angular/forms'; // NgModel lives here
+import { HttpModule } from '@angular/http';
 
+import { AppRoutingModule } from './app-routing.module';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 import { AppComponent } from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroesComponent } from './heroes.component';
-import { HeroService } from './hero.service';
 import { DashboardComponent } from './dashboard.component';
-import { AppRoutingModule } from './app-routing.module';
+import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
+
+
 
 @NgModule({
   declarations: [
